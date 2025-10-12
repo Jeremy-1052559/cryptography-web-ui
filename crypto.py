16c1797da9ee487fd6be2752568cf17f0db52d03
Jeremy-1052559 <1052559@hr.nl>
2025-10-12 19:29:18 +0200

    Update crypto.py
    Add secret key generation method

--- crypto.py
+++ crypto.py
@@ -1,5 +1,8 @@
 import pathlib
 
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
 
 def get_env_path():
     path = pathlib.Path(__file__)
@@ -19,6 +22,11 @@ def get_env():
                 continue
             env_dict[item[0]] = item[1]
     return env_dict
+
+
+def get_secret_key(password: str, salt: bytes):
+    kdf = PBKDF2HMAC(hashes.SHA256(), length=32, salt=salt, iterations=100000)
+    return kdf.derive(password.encode())
 
 
 def encrypt(message: str, password: str):
